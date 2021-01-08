be3d1738b094aa1f5ea0ad48adc34c1fe50e8d04
Simon <silvestresimon@icloud.com>
2021-01-08 18:07:24 +0100

    card

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <div id="app" :style='{ background: "url(" + image + ") no-repeat bottom right, url(" + image2 + ") #191a24" }'>
+  <div id="app" :style='{ background: "url(" + image + ") no-repeat bottom right / contain, url(" + image2 + ") #201f2b" }'>
     <h1>we're launching soon</h1>
     <SocialMedia />
+    <div class="temps">
+      <Horaires :chiffre="Days" name="days" />
+      <Horaires :chiffre="Hours" name="hours" />
+      <Horaires :chiffre="Minutes" name="minutes" />
+      <Horaires :chiffre="Seconds" name="seconds" />
+    </div>
   </div>
 </template>
 
 <script>
 import SocialMedia from './components/SocialMedia.vue'
+import Horaires from './components/Horaires.vue'
 
 export default {
   name: 'App',
   components: {
-    SocialMedia
+    SocialMedia,
+    Horaires
   },
   data() {
     return {
       image: require('@/assets/pattern-hills.svg'),
       image2: require('@/assets/bg-stars.svg'),
-      Days: 0,
-      Hours: 0,
-      Minutes: 0,
-      Seconds: 0,
+      Days: '08',
+      Hours: '23',
+      Minutes: '55',
+      Seconds: '41',
     }
   }
 }
@@ -39,16 +47,22 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     width: 100%;
     height: 100vh;
-    background-color:  hsl(234, 17%, 12%);
+    background-color:  #201f2b;
   }
   h1 {
     width: fit-content;
     position: absolute;
     top: 120px;
-    left: calc(50% - 233px);
+    left: calc(50% - 212.5px);
     letter-spacing: 5px;
     text-transform: uppercase;
     color: #fff;
-    font-size: 28px;
+    font-size: 25px;
+  }
+  .temps {
+    display: flex;
+    position: absolute;
+    top: 260px;
+    left: calc(50% - 360px);
   }
 </style>
---
+++ src/components/Horaires.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="horaires">
+        <div class="card">
+            <div class="haut">
+
+            </div>
+            <div class="bas">
+
+            </div>
+            <span class="nombre" :data-letters="chiffre">{{ chiffre }}</span>
+        </div>
+        <p class="name">{{ name }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        chiffre: String,
+        name: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .horaires {
+        margin: 0 15px;
+        .card {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            width: 150px;
+            height: 140px;
+            color: hsl(345, 95%, 68%);
+            box-shadow: 0px 8px 0px 0px #191b26;
+
+            &, .haut, .bas {
+                border-radius: 8px;
+            }
+
+            .haut {
+                position: absolute;
+                top: 0;
+                width: 100%;
+                height: 70px;
+                background-color: #2c2c45;
+            }
+            .bas {
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                height: 70px;
+                background-color: #33364f;
+            }
+            .nombre {
+                font-size: 80px;
+                z-index: 1;
+            }
+            .nombre:after {
+                content: attr(data-letters);
+                z-index: 2;
+                color: #d4526e;
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                top: 0;
+                left: 0;
+                clip-path: polygon(0 0, 100% 0, 100% 50%, 0 50%);
+            }
+            &:before, &:after {
+                content: '';
+                position: absolute;
+                width: 7px;
+                height: 14px;
+                background-color: #191b26;
+                z-index: 1;
+            }
+            &:before {
+                top: calc(50% - 7px);
+                left: 0px;
+                border-radius: 0 50px 50px 0;
+            }
+            &:after {
+                top: calc(50% - 7px);
+                right: 0px;
+                border-radius: 50px 0 0 50px;
+            }
+        }
+        .name {
+            text-transform: uppercase;
+            color: hsl(237, 18%, 59%);
+            font-size: 14px;
+            letter-spacing: 5px;
+            margin: 0 auto;
+            text-align: center;
+            margin-top: 20px;
+        }
+    }
+</style>
